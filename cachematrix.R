848e87ff2804020d1c14e6cb14a2770361ce0c99
jon.h <jon.holman@hotmail.com>
2016-08-06 13:45:14 -0400

    Filled out skeleton functions to completion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions take an invertible matrix as an input, store  
+## the matrix, compute its inverse, and store this inverse
 
-## Write a short comment describing this function
-
+## This function build a vector of functions to manipulate
+## and store the data noted above
 makeCacheMatrix <- function(x = matrix()) {
-
+     M <- NULL
+     set <- function(Y) {
+          X <<- Y
+          M <<- NULL
+     }
+     get <- function() X
+     setinverse <- function(inverse) M <<- inverse
+     getinverse <- function() M
+     list(set = set, get = get,
+          setinverse = setinverse,
+          getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function retrieves the inverse of the matrix if it has
+## been computed or otherwise computes it directly
+cacheSolve <- function(X, ...) {
+     M <- X$getinverse()
+     if(!is.null(M)) {
+          message("getting cached data")
+          return(M)
+     }
+     data <- X$get()
+     M <- solve(data, ...)
+     X$setinverse(M)
+     return(M)
 }
